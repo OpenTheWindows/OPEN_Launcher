9eb6fcb4fef5c055cc09b2ef929d0362b33d7ba5
Daniela Noveska <dnoveska@endava.net>
2016-03-24 10:46:18 +0100

    UserSettingsService Unit Tests

--- src/app/components/userSettings/UserSettingsColorsService.spec.ts
+++ src/app/components/userSettings/UserSettingsColorsService.spec.ts
@@ -1,4 +1,6 @@
 import {UserSettingsColorsService} from './UserSettingsColorsService';
+import {PointerColor, BackgroundColor} from '../../shared/enums/UserSettingsEnums';
+
 
 describe('UserSettingsColorsService', function() {
   var instance: UserSettingsColorsService= null;
@@ -6,11 +8,19 @@ describe('UserSettingsColorsService', function() {
   beforeEach(() => {
     instance = new UserSettingsColorsService();
   });
-  
-  it('UserSettingsColorsService_getPointerColors', function(){
-      //Assert
-      expect(instance.getPointerColors.length).toEqual(1);
-  });
-//unit testing
 
+  it('UserSettingsColorsService_getPointerColors_ForBlackWhiteColor', function(){
+      //Assert
+      var pointerColors = instance.getPointerColors(BackgroundColor.BlackAndWhite);
+      
+      expect(pointerColors.length).toEqual(2);
+  });
+  
+   it('UserSettingsColorsService_getPointerColors_ForInColor', function(){
+      //Assert
+      var inColorAvailablePointerColor = instance.getPointerColors(BackgroundColor.InColor);
+      
+      expect(inColorAvailablePointerColor.length).toEqual(5);
+  });
+  
 })
